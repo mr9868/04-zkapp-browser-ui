dd393398377b213af395fe1e4636e1d4d64e3d42
mr9868 <nzahrotulhayati@gmail.com>
2022-12-09 02:37:50 +0000

    Deploy gh-pages

--- _next/static/chunks/webpack-1d24b98c69cfb7cb.js
+++ _next/static/chunks/webpack-1d24b98c69cfb7cb.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "97a3e63b26f3f58e" + ".css";
+/******/ 			return "static/css/" + "0f59e2f5eae0dc21" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
